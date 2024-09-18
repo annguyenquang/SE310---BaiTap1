84418293623a8609a738ffbcec95ed763c3404a9
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-09-18 21:21:51 +0700

    Update: update raw ui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,25 +28,130 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.giveBirthBtn = new System.Windows.Forms.Button();
+			this.goatInput = new System.Windows.Forms.TextBox();
+			this.sheepInput = new System.Windows.Forms.TextBox();
+			this.cowInput = new System.Windows.Forms.TextBox();
+			this.makeSound = new System.Windows.Forms.Button();
+			this.analyzeBtn = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// giveBirthBtn
+			// 
+			this.giveBirthBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.giveBirthBtn.Location = new System.Drawing.Point(37, 241);
+			this.giveBirthBtn.Name = "giveBirthBtn";
+			this.giveBirthBtn.Size = new System.Drawing.Size(117, 35);
+			this.giveBirthBtn.TabIndex = 1;
+			this.giveBirthBtn.Text = "Đẻ";
+			this.giveBirthBtn.UseVisualStyleBackColor = true;
+			// 
+			// goatInput
+			// 
+			this.goatInput.Location = new System.Drawing.Point(86, 153);
+			this.goatInput.Name = "goatInput";
+			this.goatInput.Size = new System.Drawing.Size(117, 20);
+			this.goatInput.TabIndex = 2;
+			// 
+			// sheepInput
+			// 
+			this.sheepInput.Location = new System.Drawing.Point(86, 111);
+			this.sheepInput.Name = "sheepInput";
+			this.sheepInput.Size = new System.Drawing.Size(117, 20);
+			this.sheepInput.TabIndex = 3;
+			// 
+			// cowInput
+			// 
+			this.cowInput.Location = new System.Drawing.Point(86, 75);
+			this.cowInput.Name = "cowInput";
+			this.cowInput.Size = new System.Drawing.Size(117, 20);
+			this.cowInput.TabIndex = 4;
+			// 
+			// makeSound
+			// 
+			this.makeSound.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.makeSound.Location = new System.Drawing.Point(200, 241);
+			this.makeSound.Name = "makeSound";
+			this.makeSound.Size = new System.Drawing.Size(117, 36);
+			this.makeSound.TabIndex = 5;
+			this.makeSound.Text = "Kêu";
+			this.makeSound.UseVisualStyleBackColor = true;
+			// 
+			// analyzeBtn
+			// 
+			this.analyzeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.analyzeBtn.Location = new System.Drawing.Point(360, 241);
+			this.analyzeBtn.Name = "analyzeBtn";
+			this.analyzeBtn.Size = new System.Drawing.Size(117, 36);
+			this.analyzeBtn.TabIndex = 6;
+			this.analyzeBtn.Text = "Phân tích";
+			this.analyzeBtn.UseVisualStyleBackColor = true;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(605, 105);
+			this.label1.BackColor = System.Drawing.SystemColors.Info;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(30, 25);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "label1";
-			this.label1.Click += new System.EventHandler(this.label1_Click);
+			this.label1.Size = new System.Drawing.Size(346, 39);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Nhập số lượng tại đây";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.SystemColors.Info;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(34, 78);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(22, 13);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Bò";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.SystemColors.Info;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(34, 111);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(23, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Dê";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.SystemColors.Info;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(34, 156);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(29, 13);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "Cừu";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.BackgroundImage = global::ToDo.Properties.Resources.liveStock;
 			this.ClientSize = new System.Drawing.Size(1365, 549);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
+			this.Controls.Add(this.analyzeBtn);
+			this.Controls.Add(this.makeSound);
+			this.Controls.Add(this.cowInput);
+			this.Controls.Add(this.sheepInput);
+			this.Controls.Add(this.goatInput);
+			this.Controls.Add(this.giveBirthBtn);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
@@ -57,7 +162,16 @@
 
 		#endregion
 
+		private System.Windows.Forms.Button giveBirthBtn;
+		private System.Windows.Forms.TextBox goatInput;
+		private System.Windows.Forms.TextBox sheepInput;
+		private System.Windows.Forms.TextBox cowInput;
+		private System.Windows.Forms.Button makeSound;
+		private System.Windows.Forms.Button analyzeBtn;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
 	}
 }
 
